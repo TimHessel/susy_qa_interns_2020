51c5fb374df47e737410f6f113528afdf32c622f
TimHessel <timotheehessel@MBP-de-Timothee.fritz.box>
2020-05-15 10:46:56 +0200

    Truncation filled with criteria (3rd try)

--- truncation_rootfile.C
+++ truncation_rootfile.C
@@ -38,6 +38,10 @@ void truncate(string data[], string path, const char* dir, string selection[]){
   //Creating root file with selected branched in a target directory
   int i = 0;
   int n_data = array_size(data);
+  int n_selection = array_size(selection);
+  if (n_data!=n_selection){
+    printf("Error : not matching data set and selection\n");
+  }
   for (i = 0; i < n_data; i++){
     //Loading root data
     string filename = (path + "skimmed_" + (data[i]).c_str());
@@ -115,40 +119,40 @@ void truncation_rootfile(){
 
   //Truncation criteria arrays for signal and background
   string sig_truncation[]  =
-  {"(Jet1Pt<1000)&&(Met<920)&&(mt<210)&&(LepPt<78)&&(HT<1600)&&(JetHBpt<800)&&(DrJetHBLep<4.8)&&(DrJetHBLep>0)&&(JetHBCSV>0.045)&&(LepEta>-2.4)",
-	"(Jet1Pt<1000)&&(Met<1050)&&(mt<228)&&(LepPt<72)&&(HT<1550)&&(JetHBpt<900)&&(DrJetHBLep<4.8)&&(DrJetHBLep>0.008)&&(JetHBCSV>0.04)&&(LepEta>-2.4)",
-  "(Jet1Pt<1060)&&(Met<1050)&&(mt<370)&&(LepPt<78)&&(HT<1560)&&(JetHBpt<865)&&(DrJetHBLep<4.9)&&(DrJetHBLep>0)&&(JetHBCSV>0.4)&&(LepEta>-2.4)&&(LepEta<2.4)",
-  "(Jet1Pt<1250)&&(Met<1050)&&(mt<330)&&(LepPt<75)&&(HT<1640)&&(JetHBpt<1000)&&(DrJetHBLep<5)&&(DrJetHBLep>0)&&(JetHBCSV>0)&&(LepEta>-2.5)&&(LepEta<2.5)",
-  "(Jet1Pt<1240)&&(Met<1340)&&(mt<285)&&(LepPt<74)&&(HT<1900)&&(JetHBpt<1200)&&(DrJetHBLep<5)&&(DrJetHBLep>0)&&(JetHBCSV>0.05)&&(LepEta>-2.35)&&(LepEta<2.4)",
-  "(Jet1Pt<1400)&&(Met<1300)&&(mt<400)&&(LepPt<69)&&(HT<1900)&&(JetHBpt<1100)&&(DrJetHBLep<5.1)&&(DrJetHBLep>0)&&(JetHBCSV>0.052)&&(LepEta>-2.5)&&(LepEta<2,4)",
-  "(Jet1Pt<1200)&&(Met<1350)&&(mt<350)&&(LepPt<74)&&(HT<1700)&&(JetHBpt<1000)&&(DrJetHBLep<5)&&(DrJetHBLep>0)&&(JetHBCSV>0.04)&&(LepEta>-2.5)&&(LepEta<2.5)",
-  "(Jet1Pt<1300)&&(Met<1375)&&(mt<250)&&(LepPt<60)&&(HT<1900)&&(JetHBpt<1080)&&(DrJetHBLep<4.7)&&(DrJetHBLep>0)&&(JetHBCSV>0.04)&&(LepEta>-2.5)&&(LepEta<2.3)",
-  "(Jet1Pt<1200)&&(Met<1300)&&(mt<260)&&(LepPt<53)&&(HT<1575)&&(JetHBpt<1000)&&(DrJetHBLep<4.9)&&(DrJetHBLep>0.15)&&(JetHBCSV>0.04)&&(LepEta>-2.4)&&(LepEta<2.4)",
-  "(Jet1Pt<1060)&&(Met<1100)&&(mt<190)&&(LepPt<45)&&(HT<1400)&&(JetHBpt<900)&&(DrJetHBLep<4.5)&&(DrJetHBLep>0.4)&&(JetHBCSV>0.045)&&(LepEta>-2.5)&&(LepEta<2.5)",
-  "(Jet1Pt<1050)&&(Met<1000)&&(mt<200)&&(LepPt<49)&&(HT<1450)&&(JetHBpt<900)&&(DrJetHBLep<4.9)&&(DrJetHBLep>0.4)&&(JetHBCSV>0.04)&&(LepEta>-2.5)&&(LepEta<2.5)",
-  "(Jet1Pt<1000)&&(Met<1000)&&(mt<220)&&(LepPt<48)&&(HT<1400)&&(JetHBpt<800)&&(DrJetHBLep<4.5)&&(DrJetHBLep>0.4)&&(JetHBCSV>0.04)&&(LepEta>-2.5)&&(LepEta<2.5)",
-  "(Jet1Pt<1050)&&(Met<1125)&&(mt<195)&&(LepPt<43)&&(HT<1375)&&(JetHBpt<820)&&(DrJetHBLep<4.5)&&(DrJetHBLep>0.4)&&(JetHBCSV>0.045)&&(LepEta>-2.4)&&(LepEta<2.3)",
-  "(Jet1Pt<1050)&&(Met<1125)&&(mt<195)&&(LepPt<43)&&(HT<1375)&&(JetHBpt<820)&&(DrJetHBLep<4.3)&&(DrJetHBLep>0.1)&&(JetHBCSV>0.0575)&&(LepEta>-2.4)&&(LepEta<2.3)",
-  "(Jet1Pt<1000)&&(Met<1150)&&(mt<250)&&(LepPt<45)&&(HT<1300)&&(JetHBpt<950)&&(DrJetHBLep<4.4)&&(DrJetHBLep>0.2)&&(JetHBCSV>0.04)&&(LepEta>-2.3)&&(LepEta<2)",
-  "(Jet1Pt<1100)&&(Met<1200)&&(mt<250)&&(LepPt<38)&&(HT<1700)&&(JetHBpt<800)&&(DrJetHBLep<4.2)&&(DrJetHBLep>0.3)&&(JetHBCSV>0)&&(LepEta>-2.2)&&(LepEta<2)",
-  "(Jet1Pt<975)&&(Met<1100)&&(mt<260)&&(LepPt<40)&&(HT<1600)&&(JetHBpt<975)&&(DrJetHBLep<4)&&(DrJetHBLep>0.2)&&(JetHBCSV>0)&&(LepEta>-2.3)&&(LepEta<2)",
-  "(Jet1Pt<950)&&(Met<1150)&&(mt<250)&&(LepPt<46)&&(HT<1350)&&(JetHBpt<1025)&&(DrJetHBLep<4)&&(DrJetHBLep>0.2)&&(JetHBCSV>0)&&(LepEta>-2.1)&&(LepEta<1.7)",
-  "(Jet1Pt<1000)&&(Met<1250)&&(mt<250)&&(LepPt<42)&&(HT<1550)&&(JetHBpt<830)&&(DrJetHBLep<4.2)&&(DrJetHBLep>0.3)&&(JetHBCSV>0.04)&&(LepEta>-2)&&(LepEta<1.7)",
-  "(Jet1Pt<1100)&&(Met<1300)&&(mt<225)&&(LepPt<32)&&(HT<1490)&&(JetHBpt<830)&&(DrJetHBLep<4.2)&&(DrJetHBLep>0)&&(JetHBCSV>0.04)&&(LepEta>-2.1)&&(LepEta<2)",
-  "(Jet1Pt<925)&&(Met<1280)&&(mt<250)&&(LepPt<42)&&(HT<1500)&&(JetHBpt<950)&&(DrJetHBLep<4.1)&&(DrJetHBLep>0)&&(JetHBCSV>0.03)&&(LepEta>-2.2)&&(LepEta<2.1)",
-  "(Jet1Pt<975)&&(Met<980)&&(mt<250)&&(LepPt<40)&&(HT<1400)&&(JetHBpt<1000)&&(DrJetHBLep<3.8)&&(DrJetHBLep>0.2)&&(JetHBCSV>0)&&(LepEta>-2.2)&&(LepEta<2)",
-  "(Jet1Pt<1025)&&(Met<1200)&&(mt<250)&&(LepPt<42)&&(HT<1450)&&(JetHBpt<900)&&(DrJetHBLep<4.1)&&(DrJetHBLep>0.15)&&(JetHBCSV>0.05)&&(LepEta>-2.2)&&(LepEta<2)"};
+  {"(Jet1Pt<1000)&&(Met<920)&&(mt<210)&&(LepPt<78)&&(HT<1600)&&(JetHBpt<800)&&(DrJetHBLep<4.8)&&(DrJetHBLep>0)&&(JetHBCSV>0.045)&&(LepEta>-2.4)", //Stop_250_220
+	"(Jet1Pt<1000)&&(Met<1050)&&(mt<228)&&(LepPt<72)&&(HT<1550)&&(JetHBpt<900)&&(DrJetHBLep<4.8)&&(DrJetHBLep>0.008)&&(JetHBCSV>0.04)&&(LepEta>-2.4)", //Stop_275_245
+  "(Jet1Pt<1060)&&(Met<1050)&&(mt<370)&&(LepPt<78)&&(HT<1560)&&(JetHBpt<865)&&(DrJetHBLep<4.9)&&(DrJetHBLep>0)&&(JetHBCSV>0.4)&&(LepEta>-2.4)&&(LepEta<2.4)", //Stop_300_270
+  "(Jet1Pt<1250)&&(Met<1050)&&(mt<330)&&(LepPt<75)&&(HT<1640)&&(JetHBpt<1000)&&(DrJetHBLep<5)&&(DrJetHBLep>0)&&(JetHBCSV>0)&&(LepEta>-2.5)&&(LepEta<2.5)", //Stop_325_295
+  "(Jet1Pt<1240)&&(Met<1340)&&(mt<285)&&(LepPt<74)&&(HT<1900)&&(JetHBpt<1200)&&(DrJetHBLep<5)&&(DrJetHBLep>0)&&(JetHBCSV>0.05)&&(LepEta>-2.35)&&(LepEta<2.4)", //Stop_350_320
+  "(Jet1Pt<1400)&&(Met<1300)&&(mt<400)&&(LepPt<69)&&(HT<1900)&&(JetHBpt<1100)&&(DrJetHBLep<5.1)&&(DrJetHBLep>0)&&(JetHBCSV>0.052)&&(LepEta>-2.5)&&(LepEta<2,4)", //Stop_375_345
+  "(Jet1Pt<1200)&&(Met<1350)&&(mt<350)&&(LepPt<74)&&(HT<1700)&&(JetHBpt<1000)&&(DrJetHBLep<5)&&(DrJetHBLep>0)&&(JetHBCSV>0.04)&&(LepEta>-2.5)&&(LepEta<2.5)", //Stop_400_370
+  "(Jet1Pt<1300)&&(Met<1375)&&(mt<250)&&(LepPt<60)&&(HT<1900)&&(JetHBpt<1080)&&(DrJetHBLep<4.7)&&(DrJetHBLep>0)&&(JetHBCSV>0.04)&&(LepEta>-2.5)&&(LepEta<2.3)", //Stop_425_395
+  "(Jet1Pt<1200)&&(Met<1300)&&(mt<260)&&(LepPt<53)&&(HT<1575)&&(JetHBpt<1000)&&(DrJetHBLep<4.9)&&(DrJetHBLep>0.15)&&(JetHBCSV>0.04)&&(LepEta>-2.4)&&(LepEta<2.4)", //Stop_450_420
+  "(Jet1Pt<1060)&&(Met<1100)&&(mt<190)&&(LepPt<45)&&(HT<1400)&&(JetHBpt<900)&&(DrJetHBLep<4.5)&&(DrJetHBLep>0.4)&&(JetHBCSV>0.045)&&(LepEta>-2.5)&&(LepEta<2.5)", //Stop_475_445
+  "(Jet1Pt<1050)&&(Met<1000)&&(mt<200)&&(LepPt<49)&&(HT<1450)&&(JetHBpt<900)&&(DrJetHBLep<4.9)&&(DrJetHBLep>0.4)&&(JetHBCSV>0.04)&&(LepEta>-2.5)&&(LepEta<2.5)", //Stop_500_470
+  "(Jet1Pt<1000)&&(Met<1000)&&(mt<220)&&(LepPt<48)&&(HT<1400)&&(JetHBpt<800)&&(DrJetHBLep<4.5)&&(DrJetHBLep>0.4)&&(JetHBCSV>0.04)&&(LepEta>-2.5)&&(LepEta<2.5)", //Stop_525_495
+  "(Jet1Pt<1050)&&(Met<1125)&&(mt<195)&&(LepPt<43)&&(HT<1375)&&(JetHBpt<820)&&(DrJetHBLep<4.5)&&(DrJetHBLep>0.4)&&(JetHBCSV>0.045)&&(LepEta>-2.4)&&(LepEta<2.3)", //Stop_550_520
+  "(Jet1Pt<1050)&&(Met<1125)&&(mt<195)&&(LepPt<43)&&(HT<1375)&&(JetHBpt<820)&&(DrJetHBLep<4.3)&&(DrJetHBLep>0.1)&&(JetHBCSV>0.0575)&&(LepEta>-2.4)&&(LepEta<2.3)", //Stop_575_545
+  "(Jet1Pt<1000)&&(Met<1150)&&(mt<250)&&(LepPt<45)&&(HT<1300)&&(JetHBpt<950)&&(DrJetHBLep<4.4)&&(DrJetHBLep>0.2)&&(JetHBCSV>0.04)&&(LepEta>-2.3)&&(LepEta<2)", //Stop_600_570
+  "(Jet1Pt<1100)&&(Met<1200)&&(mt<250)&&(LepPt<38)&&(HT<1700)&&(JetHBpt<800)&&(DrJetHBLep<4.2)&&(DrJetHBLep>0.3)&&(JetHBCSV>0)&&(LepEta>-2.2)&&(LepEta<2)", //Stop_625_595
+  "(Jet1Pt<975)&&(Met<1100)&&(mt<260)&&(LepPt<40)&&(HT<1600)&&(JetHBpt<975)&&(DrJetHBLep<4)&&(DrJetHBLep>0.2)&&(JetHBCSV>0)&&(LepEta>-2.3)&&(LepEta<2)", //Stop_650_620
+  "(Jet1Pt<950)&&(Met<1150)&&(mt<250)&&(LepPt<46)&&(HT<1350)&&(JetHBpt<1025)&&(DrJetHBLep<4)&&(DrJetHBLep>0.2)&&(JetHBCSV>0)&&(LepEta>-2.1)&&(LepEta<1.7)", //Stop_675_645
+  "(Jet1Pt<1000)&&(Met<1250)&&(mt<250)&&(LepPt<42)&&(HT<1550)&&(JetHBpt<830)&&(DrJetHBLep<4.2)&&(DrJetHBLep>0.3)&&(JetHBCSV>0.04)&&(LepEta>-2)&&(LepEta<1.7)", //Stop_700_670
+  "(Jet1Pt<1100)&&(Met<1300)&&(mt<225)&&(LepPt<32)&&(HT<1490)&&(JetHBpt<830)&&(DrJetHBLep<4.2)&&(DrJetHBLep>0)&&(JetHBCSV>0.04)&&(LepEta>-2.1)&&(LepEta<2)", //Stop_725_695
+  "(Jet1Pt<925)&&(Met<1280)&&(mt<250)&&(LepPt<42)&&(HT<1500)&&(JetHBpt<950)&&(DrJetHBLep<4.1)&&(DrJetHBLep>0)&&(JetHBCSV>0.03)&&(LepEta>-2.2)&&(LepEta<2.1)", //Stop_750_720
+  "(Jet1Pt<975)&&(Met<980)&&(mt<250)&&(LepPt<40)&&(HT<1400)&&(JetHBpt<1000)&&(DrJetHBLep<3.8)&&(DrJetHBLep>0.2)&&(JetHBCSV>0)&&(LepEta>-2.2)&&(LepEta<2)", //Stop_775_745
+  "(Jet1Pt<1025)&&(Met<1200)&&(mt<250)&&(LepPt<42)&&(HT<1450)&&(JetHBpt<900)&&(DrJetHBLep<4.1)&&(DrJetHBLep>0.15)&&(JetHBCSV>0.05)&&(LepEta>-2.2)&&(LepEta<2)"}; //Stop_800_770
 
   string bkg_truncation[]  =
-  {"",
-	"(Jet1Pt<700)&&(Met<800)&&(mt<700)&&(LepPt<130)&&(HT<800)&&(JetHBpt<750)&&(JetHBCSV>0)",
-  "(Jet1Pt<505)&&(Met<575)&&(mt<900)&&(LepPt<220)&&(HT<640)&&(JetHBpt<505)&&(JetHBCSV>0)&&(DrJetHBLep>0.02)",
-  "(Jet1Pt<700)&&(Met<675)&&(mt<650)&&(LepPt<355)&&(HT<900)&&(JetHBpt<680)&&(DrJetHBLep<5.3)&&(DrJetHBLep>0)&&(JetHBCSV>0)&&(LepEta>-2.5)&&(LepEta<2.5)",
-  "(Jet1Pt<925)&&(Met<925)&&(mt<1050)&&(LepPt<560)&&(HT<1300)&&(JetHBpt<960)&&(DrJetHBLep<5.6)&&(DrJetHBLep>0.2)&&(JetHBCSV>0)&&(LepEta>-2.5)&&(LepEta<2.5)",
-  "(Jet1Pt<1250)&&(Met<1200)&&(mt<1275)&&(LepPt<880)&&(HT<1675)&&(JetHBpt<1300)&&(DrJetHBLep<5.1)&&(DrJetHBLep>0.2)&&(JetHBCSV>0)&&(LepEta>-2.5)&&(LepEta<2.5)",
-  "(Jet1Pt>180)&&(Jet1Pt<2300)&&(Met<1860)&&(mt<1450)&&(LepPt<1450)&&(HT<2700)&&(JetHBpt<2250)&&(DrJetHBLep<5.1)&&(DrJetHBLep>0.2)&&(JetHBCSV>0)&&(LepEta>-2.5)&&(LepEta<2.5)",
-  "(Jet1Pt>540)&&(Jet1Pt<2800)&&(Met<2700)&&(mt<1550)&&(LepPt<2175)&&(HT>700)&&(HT<4700)&&(JetHBpt<3100)&&(DrJetHBLep<5)&&(DrJetHBLep>0)&&(JetHBCSV>0)&&(LepEta>-2.4)&&(LepEta<2.5)",
-  "(Jet1Pt<1100)&&(Met<800)&&(mt<700)&&(LepPt<500)&&(HT<2000)&&(JetHBpt<900)&&(DrJetHBLep<4.6)&&(DrJetHBLep>0.1)&&(JetHBCSV>0.04)"};
+  {"", //Wjets_70to100
+	"(Jet1Pt<700)&&(Met<800)&&(mt<700)&&(LepPt<130)&&(HT<800)&&(JetHBpt<750)&&(JetHBCSV>0)", //Wjets_100to200
+  "(Jet1Pt<505)&&(Met<575)&&(mt<900)&&(LepPt<220)&&(HT<640)&&(JetHBpt<505)&&(JetHBCSV>0)&&(DrJetHBLep>0.02)", //Wjets_200to400
+  "(Jet1Pt<700)&&(Met<675)&&(mt<650)&&(LepPt<355)&&(HT<900)&&(JetHBpt<680)&&(DrJetHBLep<5.3)&&(DrJetHBLep>0)&&(JetHBCSV>0)&&(LepEta>-2.5)&&(LepEta<2.5)", //Wjets_400to600
+  "(Jet1Pt<925)&&(Met<925)&&(mt<1050)&&(LepPt<560)&&(HT<1300)&&(JetHBpt<960)&&(DrJetHBLep<5.6)&&(DrJetHBLep>0.2)&&(JetHBCSV>0)&&(LepEta>-2.5)&&(LepEta<2.5)", //Wjets_600to800
+  "(Jet1Pt<1250)&&(Met<1200)&&(mt<1275)&&(LepPt<880)&&(HT<1675)&&(JetHBpt<1300)&&(DrJetHBLep<5.1)&&(DrJetHBLep>0.2)&&(JetHBCSV>0)&&(LepEta>-2.5)&&(LepEta<2.5)", //Wjets_800to1200
+  "(Jet1Pt>180)&&(Jet1Pt<2300)&&(Met<1860)&&(mt<1450)&&(LepPt<1450)&&(HT<2700)&&(JetHBpt<2250)&&(DrJetHBLep<5.1)&&(DrJetHBLep>0.2)&&(JetHBCSV>0)&&(LepEta>-2.5)&&(LepEta<2.5)", //Wjets_1200to2500
+  "(Jet1Pt>540)&&(Jet1Pt<2800)&&(Met<2700)&&(mt<1550)&&(LepPt<2175)&&(HT>700)&&(HT<4700)&&(JetHBpt<3100)&&(DrJetHBLep<5)&&(DrJetHBLep>0)&&(JetHBCSV>0)&&(LepEta>-2.4)&&(LepEta<2.5)", //Wjets_2500toInf
+  "(Jet1Pt<1100)&&(Met<800)&&(mt<700)&&(LepPt<500)&&(HT<2000)&&(JetHBpt<900)&&(DrJetHBLep<4.6)&&(DrJetHBLep>0.1)&&(JetHBCSV>0.04)"}; //TT_pow
   //Creating target directory
   const char* directory_name = "truncated_data/";
   mkdir(directory_name, 0777);
