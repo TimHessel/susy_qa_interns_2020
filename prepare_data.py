1fed102d85ec24261eb81f8f3ec02d211fbceaad
arturu043 <arturu043@linux.com>
2020-05-15 16:00:08 +0200

    DF  to csv 2

--- prepare_data.py
+++ prepare_data.py
@@ -6,51 +6,51 @@ import os
 #Path to data
 stop_path = "/home/t3cms/thessel/truncated_data/"
 #Signal data to be imported
-data_sig = ["T2DegStop_250_220.root",
-            "T2DegStop_275_245.root",
-            "T2DegStop_300_270.root",
-            "T2DegStop_325_295.root",
-            "T2DegStop_350_320.root",
-            "T2DegStop_375_345.root",
-            "T2DegStop_400_370.root",
-            "T2DegStop_425_395.root",
-            "T2DegStop_450_420.root",
-            "T2DegStop_475_445.root",
-            "T2DegStop_400_370.root",
-            "T2DegStop_425_395.root",
-            "T2DegStop_450_420.root",
-            "T2DegStop_475_445.root",
-            "T2DegStop_500_470.root",
-            "T2DegStop_525_495.root",
-            "T2DegStop_550_520.root",
-            "T2DegStop_575_545.root",
-            "T2DegStop_600_570.root",
-            "T2DegStop_625_595.root",
-            "T2DegStop_650_620.root",
-            "T2DegStop_675_645.root",
-            "T2DegStop_700_670.root",
-            "T2DegStop_725_695.root",
-            "T2DegStop_750_720.root",
-            "T2DegStop_775_745.root",
-            "T2DegStop_800_770.root"
+data_sig = ["truncated_T2DegStop_250_220.root",
+            "truncated_T2DegStop_275_245.root",
+            "truncated_T2DegStop_300_270.root",
+            "truncated_T2DegStop_325_295.root",
+            "truncated_T2DegStop_350_320.root",
+            "truncated_T2DegStop_375_345.root",
+            "truncated_T2DegStop_400_370.root",
+            "truncated_T2DegStop_425_395.root",
+            "truncated_T2DegStop_450_420.root",
+            "truncated_T2DegStop_475_445.root",
+            "truncated_T2DegStop_400_370.root",
+            "truncated_T2DegStop_425_395.root",
+            "truncated_T2DegStop_450_420.root",
+            "truncated_T2DegStop_475_445.root",
+            "truncated_T2DegStop_500_470.root",
+            "truncated_T2DegStop_525_495.root",
+            "truncated_T2DegStop_550_520.root",
+            "truncated_T2DegStop_575_545.root",
+            "truncated_T2DegStop_600_570.root",
+            "truncated_T2DegStop_625_595.root",
+            "truncated_T2DegStop_650_620.root",
+            "truncated_T2DegStop_675_645.root",
+            "truncated_T2DegStop_700_670.root",
+            "truncated_T2DegStop_725_695.root",
+            "truncated_T2DegStop_750_720.root",
+            "truncated_T2DegStop_775_745.root",
+            "truncated_T2DegStop_800_770.root"
             ]
 #Background data to be imported
-data_bkg = ["Wjets_70to100.root",
-            "Wjets_100to200.root",
-            "Wjets_200to400.root",
-            "Wjets_400to600.root",
-            "Wjets_600to800.root",
-            "Wjets_800to1200.root",
-            "Wjets_1200to2500.root",
-            "Wjets_2500toInf.root",
-            "TT_pow.root",
-            "ZJetsToNuNu_HT100to200.root",
-            "ZJetsToNuNu_HT200to400.root",
-            "ZJetsToNuNu_HT400to600.root",
-            "ZJetsToNuNu_HT600to800.root",
-            "ZJetsToNuNu_HT800to1200.root",
-            "ZJetsToNuNu_HT1200to2500.root",
-            "ZJetsToNuNu_HT2500toInf.root"
+data_bkg = ["truncated_Wjets_70to100.root",
+            "truncated_Wjets_100to200.root",
+            "truncated_Wjets_200to400.root",
+            "truncated_Wjets_400to600.root",
+            "truncated_Wjets_600to800.root",
+            "truncated_Wjets_800to1200.root",
+            "truncated_Wjets_1200to2500.root",
+            "truncated_Wjets_2500toInf.root",
+            "truncated_TT_pow.root",
+            "truncated_ZJetsToNuNu_HT100to200.root",
+            "truncated_ZJetsToNuNu_HT200to400.root",
+            "truncated_ZJetsToNuNu_HT400to600.root",
+            "truncated_ZJetsToNuNu_HT600to800.root",
+            "truncated_ZJetsToNuNu_HT800to1200.root",
+            "truncated_ZJetsToNuNu_HT1200to2500.root",
+            "truncated_ZJetsToNuNu_HT2500toInf.root"
             ]
 
 #Variables to be imported
@@ -89,6 +89,6 @@ def data_loader(data_path, data, data_features, XS_norm) :
 
 
 sig = data_loader(stop_path, data_sig, stop_branches, False)
-loaded_data.to_csv("signal.csv" , index =False)
+loaded_data.to_csv("signal.csv" , index =False, encoding ="utf-8")
 bkg = data_loader(stop_path, bkg_sig, stop_branches, True)
-loaded_data.to_csv("background.csv" , index =False)
+loaded_data.to_csv("background.csv" , index =False, encoding ="utf-8")
