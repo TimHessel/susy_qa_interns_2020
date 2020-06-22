5f704461d0e253c4dd5b50236635a3fc2de7b22e
Timothee Gaspard Boris Hessel <thessel@cassini01.ncg.ingrid.pt>
2020-06-22 12:02:35 +0100

    Root to CSV converter into train/test sample for signal and background using dataframe (WORKING)

--- root_to_test_train.py
+++ root_to_test_train.py
@@ -1,3 +1,14 @@
+###########################
+#Converter from root to CSV
+#22/06/2020
+#Using pandas dataframes, load every root tuple and prepare
+#a training sample and a testing sample for signal and
+#background. Normalising training signal by number of event
+#and training background by number of event and cross section.
+#WORKING
+##########################
+
+
 import numpy as np
 import pandas as pd
 import root_numpy
@@ -32,7 +43,7 @@ data_train_sig = ["T2DegStop_250_220.root",
             "T2DegStop_800_770.root"
             ]
 
-data_test_sig = ["T2Deg_Stop_550_520.root"]
+data_test_sig = ["T2DegStop_550_520.root"]
 #Background data to be imported
 data_main_bkg = [#"Wjets_70to100.root",
         #"Wjets_100to200.root",
@@ -42,7 +53,7 @@ data_main_bkg = [#"Wjets_70to100.root",
         "Wjets_800to1200.root",
         "Wjets_1200to2500.root",
         #"Wjets_2500toInf.root",
-        "TT_pow.root"#,
+        "TT_pow.root",#,
         #"ZJetsToNuNu_HT100to200.root",
         "ZJetsToNuNu_HT200to400.root",
         "ZJetsToNuNu_HT400to600.root",
@@ -68,7 +79,7 @@ data_minor_bkg = ["Wjets_70to100.root",
         "DYJetsToLL_M5to50_HT200to400.root",
         "DYJetsToLL_M5to50_HT400to600.root",
         "DYJetsToLL_M5to50_HT600toInf.root",
-        "TBar_tch_powheg.root",
+        #"TBar_tch_powheg.root",
         "TBar_tWch_ext.root",
         "T_tch_powheg.root",
         "T_tWch_ext.root"
@@ -77,40 +88,33 @@ data_minor_bkg = ["Wjets_70to100.root",
 
 def data_loader(data_path, data, XS_norm) :
     #Convert root data into matrix
-    loaded_data = None
-    n_event = []
     data_tag = []
     numbers_of_letters = 4
     for name in data :
-        print("Loading " + name)
         #Converting root data as matrixes
         data_tmp = root_numpy.root2array(data_path + "truncated_" +name,treename="bdttree")
-        data_tmp = pd.DataFrame(data_tmp, ignore_index=True)
-        n_tmp = len(data_tmp)
-        #Appending event number array
-        for evt in range(n_tmp) :
-            n_event.append(n_tmp)
-            #Concatening loaded data
-            if loaded_data is None :
-                loaded_data = data_tmp
-            else :
-                loaded_data.append(data_tmp)
-            #Building an array of corresponding original data set for each event
-            j = 0
-            for j in range(len(data_tmp)):
-                data_tag.append(name[:numbers_of_letters])
+        data_tmp = pd.DataFrame(data_tmp)
+        #Concatening loaded data
+        if name == data[0] :
+            loaded_data = data_tmp
+        else :
+            loaded_data = loaded_data.append(data_tmp, ignore_index = True)
+        #Building an array of corresponding original data set for each event
+        j = 0
+        for j in range(len(data_tmp)):
+            data_tag.append(name[:numbers_of_letters])
     #nevent_indice = 1 #indice of number of events in numpy matrices
     #xs_indice = 2 #indice of cross section in numpy matrices
     i = 0
     weights = [[0]]*len(loaded_data)
     #Weights calculation
     for i in range(len(loaded_data)) :
-        if XS_norm = True :
-            weights[i] = loaded_data.XS[i]*1000000/loaded_data.NEvent[i]
+        if XS_norm == True :
+            weights[i] = loaded_data.XS[i]*1000000/loaded_data.Nevt[i]
         else :
-            weights[i] = 1000000/loaded_data.NEvent[i]
+            weights[i] = 1000000/loaded_data.Nevt[i]
     loaded_data["Tag"] = data_tag
-    return loaded_data, weights, data_label
+    return loaded_data, weights
 
 def data_crafter(data, data_weights) :
         #Randomly picking a large amount of events from data according to corresponding weights
@@ -137,63 +141,73 @@ def data_crafter(data, data_weights) :
                         evt += 1
                 j+=1
         k = 0
-        picked_list = None
-        #Appending array with picked data
-        for k in picked_list :
-                if picked_list == None :
-                    picked_list = data[k]
-                else :
-                    picked_data.append(data[k], ignore_index=True)
+        picked_data = data.loc[picked_list].reset_index(drop=True)
         return picked_data
 
 def interpolating(data1, data2, data3, data4) :
         #Interpolation function of two data set to be distributed between -1 and 1
         data = [data1, data2, data3, data4]
-        full_data = data1.append(data2, data3, data4, ignore_index=True)
+	output_data = [data1.copy(), data2.copy(), data3.copy(), data4.copy()]
+        full_data = data1.append(data2, ignore_index=True)
+        full_data = full_data.append(data3, ignore_index=True)
+        full_data = full_data.append(data4, ignore_index=True)
         #Loop accross each variables
-        var = 0
-        for var in range(3,len(data1[0])) :
+        var_ind = 0
+        for var_ind in range(2,len(data1.columns)-1) :
                 #Calculating max and min values
+		var = data1.columns[var_ind]
                 max_value = max(full_data[var])
                 min_value = min(full_data[var])
                 #Applying linear transformation to all events
                 i = 0
-                for i in range(len(data))
+		print("Interpolating " + var)
+                for i in range(len(data)) :
                     j = 0
-                    for j in range(len(data1)) :
-                        data[var][k] = (2*data[var][k]-max_value-min_value)/(max_value-min_value)
-        return tuple(data)
+                    for j in range(len(data[i])) :
+			old_val = data[i].at[j, var]
+                        new_val = (2*old_val-max_value-min_value)/(max_value-min_value)
+			output_data[i].at[j, var] = new_val/12
+        return tuple(output_data)
 
 #Creating target folder
 csv_folder = "stop_data/"
-os.mkdir(csv_folder)
+if not os.path.exists(csv_folder) :
+	os.mkdir(csv_folder)
 
 #Converting data to numpy
-print("Loading data")
+print("Loading train signal data")
 train_sig, train_sig_weights = data_loader(stop_path, data_train_sig, False)
+print("Loading test signal data")
 test_sig, _ = data_loader(stop_path, data_test_sig, False)
+print("Loading main background data")
 bkg_main, bkg_main_weights = data_loader(stop_path, data_main_bkg, True)
+print("Loading minor background data")
 bkg_minor, _ = data_loader(stop_path, data_minor_bkg, False)
-print("Data loaded")
+print("All data loaded")
 #Picking signal events according to calculated weights
 print("Preparing signal sample")
 picked_train_sig = data_crafter(train_sig, train_sig_weights)
+print("Train sig : " + str(len(picked_train_sig)))
 picked_test_sig = test_sig.sample(frac=1).reset_index(drop=True)
+print("Test sig : " + str(len(picked_test_sig)))
 print("Preparing background sample")
 bkg_main = bkg_main.sample(frac=1).reset_index(drop=True)
 bkg_limit = len(bkg_main)/2
-picked_train_bkg = bkg_main[:bkg_limit]
+print("Main bkg : " + str(len(bkg_main)))
+picked_train_bkg = bkg_main[:bkg_limit].copy()
 picked_train_bkg = data_crafter(picked_train_bkg, bkg_main_weights[:bkg_limit])
-picked_test_bkg = bkg_main[bkg_limit:].append(bkg_minor, ignore_index=True)
+print("Train bkg : " + str(len(picked_train_bkg)))
+picked_test_bkg = bkg_main[bkg_limit:].copy().reset_index(drop=True)
+picked_test_bkg = picked_test_bkg.append(bkg_minor, ignore_index=True)
+print("Minor bkg : " + str(len(bkg_minor)))
 picked_test_bkg = picked_test_bkg.sample(frac=1).reset_index(drop=True)
+print("Test bkg : " + str(len(picked_test_bkg)))
 #Interpolating variables in final files
-print("Interpolating values")
 output_train_sig, output_train_bkg, output_test_sig, output_test_bkg = interpolating(picked_train_sig, picked_train_bkg, picked_test_sig, picked_test_bkg)
 #Writing CSV file in folder
-print("Writing CSV file")
-output_train_sig.to_csv(csv_folder + 'stop_train_sig.csv')
-output_train_bkg.to_csv(csv_folder + 'stop_train_bkg.csv')
-output_test_sig.to_csv(csv_folder + 'stop_test_sig.csv')
-output_test_bkg.to_csv(csv_folder + 'stop_test_bkg.csv')
+print("Writing CSV files")
+output_train_sig.to_csv(csv_folder + 'stop_train_sig.csv', index=False, header=False)
+output_train_bkg.to_csv(csv_folder + 'stop_train_bkg.csv', index=False, header=False)
+output_test_sig.to_csv(csv_folder + 'stop_test_sig.csv', index=False, header=False)
+output_test_bkg.to_csv(csv_folder + 'stop_test_bkg.csv', index=False, header=False)
 print("CSV files saved")
--- INSERT --                                                                       58,37         14%
